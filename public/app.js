7e45759c658884aa7f9333f37ddbeda5a55de27e
danisharsalan <danisharsalan@yahoo.com>
2018-01-09 19:56:16 -0500

    finished basic blog

--- public/app.js
+++ public/app.js
@@ -5,10 +5,49 @@
     
     function BlogController($scope, $http){
         $scope.createPost = createPost;
+        $scope.deletePost = deletePost;
+        $scope.editPost = editPost;
+        $scope.updatePost = updatePost;
+
+        function init(){
+            getAllPosts();
+        }
+        init();
+
+        function updatePost(post){
+            console.log(post);
+            $http
+                .put("/api/blogpost/" + post._id, post)
+                .success(getAllPosts);
+        }
+
+        function editPost(postID){
+            $http
+                .get("/api/blogpost/" + postID)
+                .success(function(post){
+                    $scope.post = post;
+                });
+        }
+
+        function deletePost(postID){
+            $http
+                .delete("/api/blogpost/" + postID)
+                .success(getAllPosts);
+        }
+
+        function getAllPosts(){
+            $http
+                .get("/api/blogpost")
+                .success(function(posts){
+                    $scope.posts = posts;
+                });
+        }
 
         function createPost(post){
             console.log(post);
-            $http.post("/api/blogpost", post);
+            $http
+                .post("/api/blogpost", post)
+                .success(getAllPosts);
         }
     }
 })();
--- server.js
+++ server.js
@@ -19,6 +19,69 @@ var PostSchema = mongoose.Schema({
 var PostModel = mongoose.model("PostModel", PostSchema);
 
 app.post("/api/blogpost", createPost);
+app.get("/api/blogpost", getAllPosts);
+app.get("/api/blogpost/:id", getPostByID);
+app.delete("/api/blogpost/:id", deletePost);
+app.put("/api/blogpost/:id", updatePost);
+
+function updatePost(req, res){
+    var postID = req.params.id;
+    var post = req.body;
+    PostModel
+        .update({_id: postID}, {
+            title: post.title,
+            body: post.body
+        })
+        .then(
+            function(status){
+                res.sendStatus(200);
+            },
+            function(error){
+                res.sendStatus(400);
+            }
+        );
+}
+
+function getPostByID(req, res){
+    var postID = req.params.id;
+    PostModel
+        .findById(postID)
+        .then(
+            function(post){
+                res.json(post);
+            },
+            function(error){
+                res.sendStatus(400);
+            }
+        );
+}
+
+function deletePost(req, res){
+    var postID = req.params.id;
+    PostModel
+        .remove({_id: postID})
+        .then(
+            function(status){
+                res.sendStatus(200);
+            },
+            function(error){
+                res.sendStatus(400);
+            }
+        );
+}
+
+function getAllPosts(req, res){
+    PostModel
+        .find()
+        .then(
+            function(posts){
+                res.json(posts);
+            },
+            function(error){
+                res.sendStatus(400);
+            }
+        );
+}
 
 function createPost(req, res){
     var post = req.body;
